84512fc0750831390f207e6722b3aed3d13690e0
Shubham Kumar Singh <63440492+shubhamkrsingh565@users.noreply.github.com>
2023-12-24 21:46:21 +0000

    Nav Section Completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import ReactDOM  from "react-dom";
 import App from "./App";
 import './index.css'
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+const container = document.querySelector("#root");
+const roots = createRoot(container);
+roots.render(<App />);
