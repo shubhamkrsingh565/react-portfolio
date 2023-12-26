4c2075b845852671d3366c33d64b25b8c2469417
Shubham Kumar Singh <63440492+shubhamkrsingh565@users.noreply.github.com>
2023-12-26 06:10:50 +0000

    Testimonials Section Completed

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,71 @@
 import React from 'react'
 import './Testimonials.css';
+import AVTR1 from '../../assets/avatar1.jpg';
+import AVTR2 from '../../assets/avatar2.jpg';
+import AVTR3 from '../../assets/avatar3.jpg';
+import AVTR4 from '../../assets/avatar4.jpg';
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: "Tina Snow",
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam culpa id quo officiis non corporis, blanditiis dolor in quod ad eligendi quia vel, consequuntur, rem ratione quas voluptas reprehenderit. Aspernatur sed magni deleniti."
+  },
+  {
+    avatar: AVTR2,
+    name: "Shatta Wale",
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam culpa id quo officiis non corporis, blanditiis dolor in quod ad eligendi quia vel, consequuntur, rem ratione quas voluptas reprehenderit. Aspernatur sed magni deleniti."
+  },
+  {
+    avatar: AVTR3,
+    name: "Kwame Despite",
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam culpa id quo officiis non corporis, blanditiis dolor in quod ad eligendi quia vel, consequuntur, rem ratione quas voluptas reprehenderit. Aspernatur sed magni deleniti."
+  },
+  {
+    avatar: AVTR4,
+    name: "Nana Ama McBrown",
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam culpa id quo officiis non corporis, blanditiis dolor in quod ad eligendi quia vel, consequuntur, rem ratione quas voluptas reprehenderit. Aspernatur sed magni deleniti."
+  }
+]
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
-  )
-} 
+    <section id='testimonials'>
+      <h5>Review from client</h5>
+      <h2>Testimonials</h2>
 
-export default Testimonials
+      <Swiper className="container testimonials_container"
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      >
+        {
+          data.map(({avatar, name, review}, index) => {
+            return (
+              <SwiperSlide key={index} className="testimonial">
+                <div className="client_avatar">
+                  <img src={avatar} alt={name} />
+                </div>
+                <h5 className='client_name'>{name}</h5>
+                <small className='client_review'>{review}</small>
+              </SwiperSlide>
+            )
+          })
+        }
+        
+      </Swiper>
+    </section>
+  )
+}
+
+export default Testimonials 
