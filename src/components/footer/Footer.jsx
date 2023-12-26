8116e1f2dcad1fcbff4ae2e17f5dae579ef878b1
Shubham Kumar Singh <63440492+shubhamkrsingh565@users.noreply.github.com>
2023-12-26 19:21:35 +0000

    Footer Section Completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,37 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-import './Footer.css';
+import './Footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/io'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer_logo'>SHUBHAM</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="https://facebook.com"><FaFacebookF/></a>
+        <a href="https://instagram.com"><FiInstagram/></a>
+        <a href="https://twitter.com"><IoLogoTwitter/></a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy; Shubham. App rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
-export default Footer
+export default Footer 
